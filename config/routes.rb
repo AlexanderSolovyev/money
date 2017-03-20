980d8d0a38c6ba16e4d9cf4184616d84d5775b3f
AlexanderSolovyev <1430@list.ru>
2017-03-20 20:57:12 +0300

    i-18n

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
 
   resources :types
   resources :trans
-  resources :cards
+  resources :cards 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
