d3ef46526b5a335a8f428ed73d649a884b3dc3a6
AlexanderSolovyev <1430@list.ru>
2017-03-27 23:24:56 +0300

    expirience

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -21,14 +21,19 @@ class PlansController < ApplicationController
     redirect_to plans_path
   end
   def report
+    @date=Date.today
   end
   def report_date
-    @date=params[:date]
+    @date=params.class
+    @date1=[params['per(1i)'].to_i,params['per(2i)'].to_i,params['per(3i)'].to_i]
     render :report
   end
   private
   def plan_params
     params.require(:plan).permit(:date, :type_id, :comment, :quality)
+  end
+  def date_params
+    params.require(:period).permit(:date)
   end
   def plan_find
     @plan=Plan.find_by_id(params[:id])
